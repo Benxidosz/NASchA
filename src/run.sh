20e68044f9ff5381e8196eba245d61cb79534661
bence <szabo.bence.sandor@gmail.com>
2021-03-22 12:37:35 +0100

    Running added.

---
+++ src/run.sh
@@ -0,0 +1,3 @@
+javac Skeleton/*.java Skeleton/controllers/*.java Skeleton/entities/*.java Skeleton/entities/children/*.java Skeleton/materials/*.java Skeleton/materials/children/*.java Skeleton/simulator/*.java Skeleton/things/*.java Skeleton/things/asteroids/*.java Skeleton/things/gate/*.java
+
+java Skeleton/*.java Skeleton/controllers/*.java Skeleton/entities/*.java Skeleton/entities/children/*.java Skeleton/materials/*.java Skeleton/materials/children/*.java Skeleton/simulator/*.java Skeleton/things/*.java Skeleton/things/asteroids/*.java Skeleton/things/gate/*.java
