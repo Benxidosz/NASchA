2655998d0dde4dcae638e9011e2b500861ff9e0f
czbalint <62812514+czbalint@users.noreply.github.com>
2021-05-09 00:06:10 +0200

    asteroid vizualizacio

--- src/Graphics/ui/game/views/asteroidView/AsteroidViewController.java
+++ src/Graphics/ui/game/views/asteroidView/AsteroidViewController.java
@@ -1,17 +1,20 @@
 package Graphics.ui.game.views.asteroidView;
 
 import Graphics.Inventory;
+import Graphics.material.Material;
 import Graphics.thing.Thing;
 import Graphics.ui.game.View;
 import Graphics.ui.game.UIController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 import java.io.IOException;
 
@@ -75,6 +78,19 @@ public class AsteroidViewController extends View {
 
         myCanvas.getGraphicsContext2D().setFill(Color.BEIGE);
         myCanvas.getGraphicsContext2D().fillRect(0, 0, myCanvas.getWidth(), myCanvas.getHeight());
+        double r = 200;
+        double centerX = canvasWrapper.getWidth() / 2;
+        double centerY = canvasWrapper.getHeight() / 2;
+        myCanvas.getGraphicsContext2D().strokeOval(centerX - r,centerY - r, 2 * r, 2 *r );
+        myCanvas.getGraphicsContext2D().strokeOval(centerX - r / 3,centerY - r / 3, 2 * (r / 3), 2 * (r / 3));
+        myCanvas.getGraphicsContext2D().strokeText("x layer left", centerX - 30, centerY - 150);
+        myCanvas.getGraphicsContext2D().setFont(new Font(15));
+        Material mat = UIController.getInstance().getSelectedThing().getCore();
+        if (mat != null)
+            myCanvas.getGraphicsContext2D().strokeText(mat.getName(), centerX-7, centerY+7);
+        else
+            myCanvas.getGraphicsContext2D().strokeText("empty", centerX-7, centerY+7);
+
     }
 
 
